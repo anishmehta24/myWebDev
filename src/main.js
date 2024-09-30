2a3e0755170ba76bda36370737fd6b810f541085
Anish Mehta <mehtaanish111@gmail.com>
2024-10-01 05:17:31 +0530

    fixed bugs

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
-import '../styles/components/featured.css';
 import '../styles/components/work.css';
 import '../styles/components/contact.css';
 import '../styles/components/footer.css';
